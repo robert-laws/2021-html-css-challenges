f483bbd0fcd0981413add5dd9f94a17e220fb27f
Robert Laws <roblaws@me.com>
2021-09-22 15:45:40 +0300

    begin test for interactivity feature

---
+++ working-projects/current/good-garms/interactivity/randomFromArray.js
@@ -0,0 +1,16 @@
+const names = [
+  'bob',
+  'max',
+  'hal',
+  'kal',
+  'jim',
+  'mel',
+  'don',
+  'ned',
+  'sal',
+  'lou',
+];
+
+const namesList = names.map((name) => `${name}`);
+
+console.log(namesList);
